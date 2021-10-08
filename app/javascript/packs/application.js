7d784c104b2e3ae1cf7dba0b7d1facd001651f2b
parth <parthajudiya96647@gmail.com>
2021-10-08 09:29:43 +0530

    add favicon

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,9 +5,14 @@
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
+import "@doabit/semantic-ui-sass"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+$(document).on('turbolinks:load', function(){
+    $('.ui.dropdown').dropdown();
+})
